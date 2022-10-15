96141cb43e7f939625ac71ea708a859f0864811b
coroner21 <chris.coroner@web.de>
2022-10-15 21:59:11 +0200

    Actual DSD U8

--- codecs/sabre32.c
+++ codecs/sabre32.c
@@ -218,7 +218,7 @@ struct sabre32_priv {
             SNDRV_PCM_FMTBIT_S24_3LE | \
             SNDRV_PCM_FMTBIT_S24_LE | \
             SNDRV_PCM_FMTBIT_S32_LE | \
-            SNDRV_PCM_FMTBIT_DSD_U8_LE | \
+            SNDRV_PCM_FMTBIT_DSD_U8 | \
             0)
 
 static const char * const sabre32_dpll_texts[] = {
