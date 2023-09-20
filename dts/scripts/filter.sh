1bf5943f63cadedccd99356cd599902f0f829412
coroner21 <chris.coroner@web.de>
2023-09-20 14:26:45 +0200

    Update DTS stuff

--- dts/scripts/filter.sh
+++ dts/scripts/filter.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-# git branch -D upstream/rewritten-prev upstream/master upstream/rewritten filter-state      
+# git branch -D upstream/rewritten-prev upstream/master upstream/rewritten filter-state-split
 
 set -e
 
 export SCRIPTS=$(dirname $(readlink -f $0))
+
+export FILTER_BRANCH_SQUELCH_WARNING=1
 
 UPSTREAM_MASTER=upstream/master
 UPSTREAM_REWRITTEN=upstream/dts
@@ -27,12 +29,12 @@ rm -f .git/refs/original/refs/heads/${UPSTREAM_REWRITTEN}
 
 git branch -f $UPSTREAM_REWRITTEN FETCH_HEAD
 
-git filter-branch --force \
+PATH=$(git --exec-path):$PATH $SCRIPTS/git-filter-branch --force \
 	--index-filter ${SCRIPTS}/index-filter.sh \
 	--msg-filter 'cat && /bin/echo -e "\n[ upstream commit: $GIT_COMMIT ]"' \
 	--tag-name-filter 'while read t ; do /bin/echo -n $t-dts-raw ; done' \
 	--parent-filter 'sed "s/-p //g" | xargs -r git show-branch --independent | sed "s/\</-p /g"' \
-	--prune-empty --state-branch refs/heads/filter-state \
+	--prune-empty --state-branch refs/heads/filter-state-split \
 	-- $RANGE
 
 git branch -f $UPSTREAM_MASTER FETCH_HEAD
--- dts/scripts/pull.sh
+++ dts/scripts/pull.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-git pull --no-edit git@github.com:beagleboard/bb.org-overlays.git master
-git pull --no-edit git@github.com:beagleboard/bb.org-overlays.git master --tags
-git pull --no-edit git@github.com:RobertCNelson/bb.org-overlays.git master
-git pull --no-edit git@github.com:RobertCNelson/bb.org-overlays.git master --tags
+git pull --no-edit https://github.com/beagleboard/BeagleBoard-DeviceTrees.git v6.18.x
+git pull --no-edit https://openbeagle.org/beagleboard/BeagleBoard-DeviceTrees.git v6.18.x
 
