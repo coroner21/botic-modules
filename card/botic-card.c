daa61b0d8edd7e90df87330cb51df332d7e8f3f5
coroner21 <chris.coroner@web.de>
2026-06-19 23:21:18 +0200

    Card driver to linux-6.18

--- card/botic-card.c
+++ card/botic-card.c
@@ -34,7 +34,7 @@ struct botic_priv {
 static int botic_hw_params(struct snd_pcm_substream *substream,
 		struct snd_pcm_hw_params *params) {
 	
-	struct snd_soc_pcm_runtime *rtd = asoc_substream_to_rtd(substream);
+	struct snd_soc_pcm_runtime *rtd = snd_soc_substream_to_rtd(substream);
 	struct snd_soc_dai *codec_dai = snd_soc_rtd_to_codec(rtd, 0);
 	struct snd_soc_dai *cpu_dai = snd_soc_rtd_to_cpu(rtd, 0);
 	struct botic_priv *priv = snd_soc_card_get_drvdata(rtd->card);
