25e3ce6b4b5bf63816e5000329d089377d6db452
coroner21 <chris.coroner@web.de>
2026-06-19 23:16:33 +0200

    Update card driver to linux 6.18

--- card/botic-card.c
+++ card/botic-card.c
@@ -34,9 +34,9 @@ struct botic_priv {
 static int botic_hw_params(struct snd_pcm_substream *substream,
 		struct snd_pcm_hw_params *params) {
 	
-	struct snd_soc_pcm_runtime *rtd = substream->private_data;
-	struct snd_soc_dai *codec_dai = asoc_rtd_to_codec(rtd, 0);
-	struct snd_soc_dai *cpu_dai = asoc_rtd_to_cpu(rtd, 0);
+	struct snd_soc_pcm_runtime *rtd = asoc_substream_to_rtd(substream);
+	struct snd_soc_dai *codec_dai = snd_soc_rtd_to_codec(rtd, 0);
+	struct snd_soc_dai *cpu_dai = snd_soc_rtd_to_cpu(rtd, 0);
 	struct botic_priv *priv = snd_soc_card_get_drvdata(rtd->card);
 	unsigned int sysclk, bclk, divisor;
 	int ret;
@@ -299,7 +299,7 @@ static int asoc_botic_card_resume(struct platform_device *pdev) {
 
 static struct platform_driver asoc_botic_card_driver = {
 	.probe = asoc_botic_card_probe,
-	.remove = asoc_botic_card_remove,
+//	.remove = asoc_botic_card_remove,
 	.shutdown = asoc_botic_card_shutdown,
 	.suspend = asoc_botic_card_suspend,
 	.resume = asoc_botic_card_resume,
